4d75064a325bfb3022d2b2a99f875fe280042205
HARSHIL1402 <harshilbhandari100@gmail.com>
2024-01-21 19:27:07 +0530

    first commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,7 +11,7 @@ export const signup = async (req, res) =>{
         }
         const hashedPassword = await bcrypt.hash(password, 12)
         const newUser = await users.create({ name, email,password:hashedPassword })
-        const token = jwt.sign({email:newUser.email, id:newUser.id},"test",{expiresIn:'1h'});
+        const token = jwt.sign({email:newUser.email, id:newUser.id},process.env.JWT_SECRET,{expiresIn:'1h'});
         res.status(200).json({result:newUser,token})
     }catch(error){
         res.status(500).json("Something went wrong...")
@@ -30,7 +30,7 @@ export const login = async (req, res) =>{
         if(!isPasswordCrt){
             return res.status(404).json({message:"Invaild credentials"})
         }
-        const token = jwt.sign({email:existinguser.email, id:existinguser.id},"test",{expiresIn:'1h'});
+        const token = jwt.sign({email:existinguser.email, id:existinguser.id},process.env.JWT_SECRET,{expiresIn:'1h'});
         res.status(200).json({result:existinguser,token})
     } catch (error) {
         res.status(500).json("Something went wrong...")
--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import express  from "express"
 import mongoose from "mongoose"
 import cors from "cors"
-// import dotenv from "dotenv"
+ import dotenv from "dotenv"
 
-import userRoutes from './server/routes/user.js'
-import questionRoutes from './server/routes/Questions.js'
-import answerRoutes from './server/routes/Answers.js'
+import userRoutes from './routes/user.js'
+import questionRoutes from './routes/Questions.js'
+import answerRoutes from './routes/Answers.js'
 const app = express();
-// dotenv.config()
+ dotenv.config()
 
 app.use(express.json({limit:"30mb", extended:true}))
 app.use(express.urlencoded({limit:"30mb", extended:true}))
@@ -23,9 +23,9 @@ app.use('/answer',answerRoutes)
 
 const PORT = process.env.PORT || 5000
 
-const CONNECTION_URL= "mongodb+srv://harshil:harshil7226@stack-overflow-clone.sa1boe5.mongodb.net/?retryWrites=true&w=majority"
+const DATABASE_URL = process.env.CONNECTION_URL
 
- mongoose.connect( CONNECTION_URL, { useNewUrlParser:true,useUnifiedTopology:true})
+ mongoose.connect(DATABASE_URL , { useNewUrlParser:true,useUnifiedTopology:true})
  .then(()=> app.listen(PORT,()=> {console.log(`server running on port ${PORT}`)}))
  .catch((err)=>console.log(err.message))
 
